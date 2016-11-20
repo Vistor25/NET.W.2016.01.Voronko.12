b57a5bdb2363a2040905d9c749ecbe30876dc964
Vistor25 <victorvoronko@gmail.com>
2016-11-20 22:41:53 +0300

    add

---
+++ Task4.ConsoleUI/BookStorage.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Task4.ConsoleUI
+{
+    public class BookStorage : IBookStorage
+    {
+        private string FilePath { get; }
+
+        public BookStorage(string path)
+        {
+            FilePath = path;
+        }
+
+        public List<Book> Load()
+        {
+            List<Book> list = new List<Book>();
+            try
+            {
+                using (BinaryReader binaryReader = new BinaryReader(File.Open(FilePath, FileMode.Open)))
+                {
+                    while (binaryReader.PeekChar() > -1)
+                    {
+                        string Title = binaryReader.ReadString();
+                        string Author = binaryReader.ReadString();
+                        int NumberOfPages = binaryReader.ReadInt32();
+                        int YearOfPublishing = binaryReader.ReadInt32();
+                        decimal Price = binaryReader.ReadDecimal();
+                        list.Add(new Book(Title, Author, NumberOfPages, YearOfPublishing, Price));
+                    }
+
+                }
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+            return list;
+        }
+
+        public void Save(List<Book> bookList)
+        {
+            try
+            {
+                using (BinaryWriter binaryWriter = new BinaryWriter(File.Open(FilePath, FileMode.OpenOrCreate)))
+                {
+                    foreach (var book in bookList)
+                    {
+                        binaryWriter.Write(book.Title);
+                        binaryWriter.Write(book.Author);
+                        binaryWriter.Write(book.NumberOfPages);
+                        binaryWriter.Write(book.YearOfPublishing);
+                        binaryWriter.Write(book.Price);
+                    }
+                }
+
+            }
+            catch (Exception e)
+            {
+
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
--- Task4.ConsoleUI/Program.cs
+++ Task4.ConsoleUI/Program.cs
@@ -10,17 +10,22 @@ namespace Task4.ConsoleUI
     {
         static void Main(string[] args)
         {
-            Book book1 = new Book("Гордость и предубеждение", "Джейн Остин", 700, 1985, (decimal)23.44);
-            Book book2=new Book("Тёмные начала", "Филип Пулман", 453, 1998, (decimal)12.45);
-            BookListService booklist = new BookListService();
-            booklist.AddBook(book1);
-            booklist.AddBook(book2);
-            booklist.SortBooksByTag(Book=>Book.Price);
+        //    Book book1 = new Book("Гордость и предубеждение", "Джейн Остин", 700, 1985, (decimal)23.44);
+        //    Book book2=new Book("Тёмные начала", "Филип Пулман", 453, 1998, (decimal)12.45);
+           BookListService booklist = new BookListService();
+        //    booklist.AddBook(book1);
+        //    booklist.AddBook(book2);
+        //    booklist.SortBooksByTag(Book=>Book.Price);
+        //    foreach (var VARIABLE in booklist)
+        //    {
+        //        Console.WriteLine(VARIABLE.ToString());
+        //    }
+        //    booklist.Save(new BookStorage("a"));
+            booklist.Load(new BookStorage("a"));
             foreach (var VARIABLE in booklist)
             {
                 Console.WriteLine(VARIABLE.ToString());
             }
-            
             Console.ReadKey();
         }
     }
